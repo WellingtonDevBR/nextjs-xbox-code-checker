52f2f561a8be4e127738aeccf07dff778aaba7d6
unknown <wellingtonsantos@faccioo.com.br>
2021-05-19 11:31:13 -0300

    update key

--- src/components/StatusBox/index.tsx
+++ src/components/StatusBox/index.tsx
@@ -56,15 +56,7 @@ export function StatusBox({ tokens, tokenAmount, keys, validKeys, invalidKeys, r
                                 <p className={token.tokenState == 'Active' ? styles.CheckMark : styles.UncheckMark}>{index}</p>
                                 <p>{keys[index]}</p>
                                 <p className={token.tokenState == 'Active' ? styles.valid : styles.invalid}>{token.tokenState}</p>
-                                <p>{keys.forEach((item) => {
-                                    const counter = 0;
-                                    
-                                    for(let i =0; 0 < keys.length; i++) {
-                                        console.log(i)
-                                    }
-
-
-                                })}</p>
+                                <p>1</p>
                             </div>
                         ))
                     }
