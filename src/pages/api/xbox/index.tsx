640185a1eb1272b67e666f44ced48f54ff2a795d
Wellington dos Santos <wellingtonsantos@faccioo.com.br>
2021-11-05 02:17:33 -0300

    update

--- src/pages/api/xbox/index.tsx
+++ src/pages/api/xbox/index.tsx
@@ -21,23 +21,23 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
-  global.localStorage = new LocalStorage("./scratch");
+//   global.localStorage = new LocalStorage("./scratch");
 
-  let authtoken = global.localStorage.getItem("auth_token");
+//   let authtoken = global.localStorage.getItem("auth_token");
 
-  const resp = await axios.get(
-    "https://purchase.mp.microsoft.com/v7.0/tokenDescriptions/YTC27-F4C4R-PTTVJ-2R4JX-VCVCZ?market=US&language=en-US&supportMultiAvailabilities=true",
-    {
-      headers: {
-        Authorization: `${authtoken}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+//   const resp = await axios.get(
+//     "https://purchase.mp.microsoft.com/v7.0/tokenDescriptions/YTC27-F4C4R-PTTVJ-2R4JX-VCVCZ?market=US&language=en-US&supportMultiAvailabilities=true",
+//     {
+//       headers: {
+//         Authorization: `${authtoken}`,
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
 
-  if (resp.status == 200) {
-    return res.status(200).json({ Authorization: authtoken });
-  }
+//   if (resp.status == 200) {
+//     return res.status(200).json({ Authorization: authtoken });
+//   }
 
   const response = await axios
     .get("http://34.227.32.18:8000/")
@@ -45,6 +45,6 @@ export default async function handler(req, res) {
       return response.data.auth_token;
     });
 
-  global.localStorage.setItem("auth_token", response);
+//   global.localStorage.setItem("auth_token", response);
   res.status(200).json({ Authorization: response });
 }
